082a5ce88ff75adb297c54f429d53a99aecd1117
Brylelariosa <brylelariosa12345@gmail.com>
2026-02-05 14:48:52 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,17 @@
-// We updated the version to v4 so the browser knows to reload everything
-const CACHE_NAME = 'manga-tool-v4'; 
-
+const CACHE_NAME = 'manga-tool-v5'; // Incrementing version
 const ASSETS = [
     './',
     './index.html',
     './manifest.json',
-    './worker.js',      // <--- IMPORTANT: New file added
-    './fflate.min.js'   // <--- IMPORTANT: New file added
+    './worker.js',
+    './fflate.min.js'
 ];
 
-// Install Event: Caches all files immediately
 self.addEventListener('install', (e) => {
-    self.skipWaiting(); // Forces this new service worker to activate right away
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-    );
+    self.skipWaiting();
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// Activate Event: Cleans up old versions (v1, v2, v3...)
 self.addEventListener('activate', (e) => {
     e.waitUntil(
         caches.keys().then((keys) => {
@@ -31,12 +25,8 @@ self.addEventListener('activate', (e) => {
     return self.clients.claim();
 });
 
-// Fetch Event: Serves files from cache when offline
 self.addEventListener('fetch', (e) => {
-    // Strategy: Cache First, falling back to Network
     e.respondWith(
-        caches.match(e.request).then((cachedResponse) => {
-            return cachedResponse || fetch(e.request);
-        })
+        caches.match(e.request).then((res) => res || fetch(e.request))
     );
 });
